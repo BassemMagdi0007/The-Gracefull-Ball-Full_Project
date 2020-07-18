d56c91e99b48050f1dd7984e4975c5fa8c40331a
BassemMagdi0007 <BassemMagdi0007@users.noreply.github.com>
2020-07-18 03:11:16 +0200

    The Gracefull Ball
    Full game version

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameController : MonoBehaviour
+{
+    private float Score = 0.0f;
+
+    public Text ScoreText;
+    public Text gameOverText;
+    public Text restartText;
+    public Text Pause;
+
+    public GameObject[] obstacles;
+    public float scrollSpeed = 10f;
+    public Transform obstaclesSpawnPoint;
+    public AudioSource audioSource;
+    public float counter = 0f;
+
+    public static bool gameOver;
+    private bool restart;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+
+        gameOverText.text = "";
+        restartText.text = "";
+        Pause.text = "";
+
+        Time.timeScale = 1;
+
+        GenerateObstacles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+            
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+
+        Score += Time.deltaTime;
+        ScoreText.text = "Score : " + ((int)Score).ToString();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Time.timeScale = 0;
+            Pause.text = "Pause";
+            audioSource.mute = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            Time.timeScale = 1;
+            Pause.text = "";
+            audioSource.mute = false;
+        }
+
+        if (counter <= 0)
+        {
+            GenerateObstacles();
+        }
+        else
+        {
+            counter -= Time.deltaTime;
+        }
+
+        GameObject CurrentChild;
+        for(int i =0; i< transform.childCount; i++)
+        {
+            CurrentChild = transform.GetChild(i).gameObject;
+            ScrollObstcles(CurrentChild);
+        }
+
+        EndGame();
+    }
+
+    void GenerateObstacles()
+    {
+        GameObject newObstacle = Instantiate(obstacles[Random.Range(0, obstacles.Length)], obstaclesSpawnPoint.position, Quaternion.identity);
+        newObstacle.transform.parent = transform;
+
+        counter = 2f;
+    }
+
+    void ScrollObstcles( GameObject currentObstcle )
+    {
+        currentObstcle.transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
+    }
+
+    
+    public void EndGame()
+    {
+        if(gameOver == true)
+        {
+            gameOverText.text = "Game Over";
+            restartText.text = "Press 'R' To Restart";
+            Time.timeScale = 0;
+            restart = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+	private float maxSpeed = 3f;
+	private float jumpForce = 650;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+	public Boundary boundary;
+	//private float verticalSpeed = 10;
+
+	[HideInInspector]
+	public GameObject Boost;
+	[HideInInspector]
+	public GameObject Cloud;
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
+	private bool isGrounded = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		Cloud = GameObject.Find("Cloud");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision2D)
+	{
+		// the difference between the two objects velocities BEFORE colliding
+		// Create the colliding cloude 
+		if (collision2D.relativeVelocity.magnitude > 15)
+		{
+			Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
+		}
+
+		if (collision2D.gameObject.CompareTag("obstacle"))
+		{
+			Debug.Log("Hit");
+			GameController.gameOver = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Check the jump action 
+		if (Input.GetButtonDown("Jump") && (isGrounded))
+			rb2d.AddForce(new Vector2(0, jumpForce));
+	}
+
+	void FixedUpdate()
+	{
+		//Horizontal Movement
+		float hor = Input.GetAxis("Horizontal");
+		anim.SetFloat("Speed", Mathf.Abs(hor));
+		rb2d.velocity = new Vector2(hor * maxSpeed, rb2d.velocity.y);
+
+		//Check Ground 
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.15F, whatIsGround);
+		anim.SetBool("IsGrounded", isGrounded);
+
+		//Boundray 
+		rb2d.position = new Vector3
+		(
+			Mathf.Clamp(rb2d.position.x, boundary.xMin, boundary.xMax),
+			transform.position.y,
+			0.0f
+		);
+	}
+}
